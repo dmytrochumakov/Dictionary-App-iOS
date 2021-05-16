41fa3b3dd0468920187e7bd3274389282cd41e03
Dmitry Chumakov <dima.chumakovwork@gmail.com>
2021-05-16 13:51:40 +0300

    added localization to project

---
+++ MyDictionary_App_Swift/Utilites/Enums/AppLanguageType.swift
@@ -0,0 +1,14 @@
+//
+//  AppLanguageType.swift
+//  MyDictionary_App_Swift
+//
+//  Created by Dmytro Chumakov on 16.05.2021.
+//
+
+import Foundation
+
+enum AppLanguageType: String, CaseIterable {
+    
+    case en = "en"
+
+}
