f3f01752328956b0257119d73319c1352c6e2876
Dmitry Chumakov <dima.chumakovwork@gmail.com>
2021-05-17 18:59:29 +0300

    added NumberOfRowsInSectionProtocol

---
+++ MyDictionary_App_Swift/Utilites/Protocols/NumberOfRowsInSectionProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  NumberOfRowsInSectionProtocol.swift
+//  MyDictionary_App_Swift
+//
+//  Created by Dmytro Chumakov on 17.05.2021.
+//
+
+import Foundation
+
+protocol NumberOfRowsInSectionProtocol {
+    func numberOfRowsInSection(_ section: Int) -> Int
+}
