394d2222feb051e08d25203941772d12af80b577
Dima <dima.chumakovwork@gmail.com>
2021-07-17 14:47:15 +0300

    added decodable protocol to wordmodel

--- MyDictionary_App_Swift/Core/Models/WordModel.swift
+++ MyDictionary_App_Swift/Core/Models/WordModel.swift
@@ -34,3 +34,28 @@ extension WordModel {
     }
     
 }
+
+extension WordModel: Decodable {
+    
+    enum CodingKeys: String, CodingKey {
+        case user_id = "user_id"
+        case id = "id"
+        case word = "word"
+        case word_description = "word_description"
+        case word_language = "word_language"
+        case created_at = "created_at"
+        case updated_at = "updated_at"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.user_id = try container.decode(Int64.self, forKey: .user_id)
+        self.id = try container.decode(Int64.self, forKey: .id)
+        self.word = try container.decode(String.self, forKey: .word)
+        self.word_description = try container.decode(String.self, forKey: .word_description)
+        self.word_language = try container.decode(String.self, forKey: .word_language)
+        self.created_at = try container.decode(String.self, forKey: .created_at)
+        self.updated_at = try container.decode(String.self, forKey: .updated_at)
+    }
+    
+}
