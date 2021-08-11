2450c500fe796deee4231944f9b2c4ac75732fd6
Dima <dima.chumakovwork@gmail.com>
2021-08-11 12:06:12 +0300

    added new colors

--- MyDictionary_App_Swift/Common/AppStyling/AppStyling.swift
+++ MyDictionary_App_Swift/Common/AppStyling/AppStyling.swift
@@ -10,26 +10,51 @@ import UIKit
 struct AppStyling {
     
     enum Color {
+                
+        // Light
+        case md_White_0_Light_Appearence
+        case md_Black_0_Light_Appearence
+        case md_Light_Gray_0_Light_Appearence
+        case md_Blue_0_Light_Appearence
         
-        case systemBlack
-        case systemWhite
-        case lightGray
-        case darkGray
-        case systemGray
+        // Dark
+        case md_White_0_Dark_Appearence
+        case md_Black_0_Dark_Appearence
+        case md_Light_Gray_0_Dark_Appearence
+        case md_Blue_0_Dark_Appearence
+        
+        
         
         /// - Parameter alpha: 1.0 by default
         func color(_ alpha: CGFloat = 1.0) -> UIColor {
             switch self {
-            case .lightGray:
-                return UIColor.lightGray.withAlphaComponent(alpha)
-            case .darkGray:
-                return UIColor.darkGray.withAlphaComponent(alpha)
-            case .systemBlack:
-                return UIColor.black.withAlphaComponent(alpha)
-            case .systemWhite:
-                return UIColor.white.withAlphaComponent(alpha)
-            case .systemGray:
-                return UIColor.systemGray.withAlphaComponent(alpha)
+            
+            // Light
+            case .md_White_0_Light_Appearence:
+                return UIColor.init(rgb: 0xFFFFFF, alpha: alpha)
+                
+            case .md_Black_0_Light_Appearence:
+                return UIColor.init(rgb: 0x101010, alpha: alpha)
+                
+            case .md_Light_Gray_0_Light_Appearence:
+                return UIColor.init(rgb: 0xF8F8F8, alpha: alpha)
+                
+            case .md_Blue_0_Light_Appearence:
+                return UIColor.init(rgb: 0x007AFF, alpha: alpha)
+                
+            // Dark
+            case .md_White_0_Dark_Appearence:
+                return UIColor.init(rgb: 0xF2F2F7, alpha: alpha)
+                
+            case .md_Black_0_Dark_Appearence:
+                return UIColor.init(rgb: 0x1C1C1E, alpha: alpha)
+                
+            case .md_Light_Gray_0_Dark_Appearence:
+                return UIColor.init(rgb: 0x48484A, alpha: alpha)
+                
+            case .md_Blue_0_Dark_Appearence:
+                return UIColor.init(rgb: 0x0A84FF, alpha: alpha)
+                
             }
         }
         
