8d90c89c4816dc6d87c55a37b300e354ade6253c
Dima <dima.chumakovwork@gmail.com>
2021-08-12 10:26:30 +0300

    added development enviroment

--- MyDictionary_App_Swift/Core/Network/Enums/MDAPIEnvironment.swift
+++ MyDictionary_App_Swift/Core/Network/Enums/MDAPIEnvironment.swift
@@ -9,10 +9,13 @@ import Foundation
 
 enum MDAPIEnvironment: MDEnvironmentProtocol {
     
+    case development
     case production
     
     var baseURL: String {
         switch self {
+        case .development:
+            return "http://localhost:8089/api/\(MDAPIVersion.v1.rawValue)/"
         case .production:
             return "https://my-dictionary-rest-api-cloud-run-service-s3h77mhuwa-uc.a.run.app/api/\(MDAPIVersion.v1.rawValue)/"
         }
