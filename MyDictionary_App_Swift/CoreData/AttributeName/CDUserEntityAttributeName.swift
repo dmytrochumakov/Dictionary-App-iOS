0179c617b3cad5376ce1228441d27bca72296e73
Dima <dima.chumakovwork@gmail.com>
2021-08-14 15:07:36 +0300

    addedCDUserEntityAttributeName

---
+++ MyDictionary_App_Swift/CoreData/AttributeName/CDUserEntityAttributeName.swift
@@ -0,0 +1,16 @@
+//
+//  CDUserEntityAttributeName.swift
+//  MyDictionary_App_Swift
+//
+//  Created by Dmytro Chumakov on 14.08.2021.
+//
+
+import Foundation
+
+struct CDUserEntityAttributeName {
+    static let userId: String = "userId"
+    static let nickname: String = "nickname"
+    static let password: String = "password"
+    static let createdAt: String = "createdAt"
+    static let updatedAt: String = "updatedAt"
+}
