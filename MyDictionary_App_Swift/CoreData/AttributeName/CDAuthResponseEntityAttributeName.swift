88ebb565a133575a82304a65e106b621b0559ba4
Dima <dima.chumakovwork@gmail.com>
2021-08-14 15:05:13 +0300

    addedCDAuthResponseEntityAttributeName

---
+++ MyDictionary_App_Swift/CoreData/AttributeName/CDAuthResponseEntityAttributeName.swift
@@ -0,0 +1,13 @@
+//
+//  CDAuthResponseEntityAttributeName.swift
+//  MyDictionary_App_Swift
+//
+//  Created by Dmytro Chumakov on 14.08.2021.
+//
+
+import Foundation
+
+struct CDAuthResponseEntityAttributeName {
+    static let accessToken: String = "accessToken"
+    static let expirationDate: String = "expirationDate"
+}
