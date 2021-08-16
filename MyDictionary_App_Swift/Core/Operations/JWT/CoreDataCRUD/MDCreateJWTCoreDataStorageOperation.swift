09a028dd69097f1969a694e68a20316d4023fd9c
Dima <dima.chumakovwork@gmail.com>
2021-08-16 09:11:33 +0300

    added MDCreateJWTCoreDataStorageOperation

---
+++ MyDictionary_App_Swift/Core/Operations/JWT/CoreDataCRUD/MDCreateJWTCoreDataStorageOperation.swift
@@ -0,0 +1,56 @@
+//
+//  MDCreateJWTCoreDataStorageOperation.swift
+//  MyDictionary_App_Swift
+//
+//  Created by Dmytro Chumakov on 16.08.2021.
+//
+
+import Foundation
+import CoreData
+
+final class MDCreateJWTCoreDataStorageOperation: MDOperation {
+    
+    fileprivate let managedObjectContext: NSManagedObjectContext
+    fileprivate let coreDataStorage: MDJWTCoreDataStorage
+    fileprivate let authResponse: AuthResponse
+    fileprivate let result: MDEntityResult<AuthResponse>?
+    
+    init(managedObjectContext: NSManagedObjectContext,
+         coreDataStorage: MDJWTCoreDataStorage,
+         authResponse: AuthResponse,
+         result: MDEntityResult<AuthResponse>?) {
+        
+        self.managedObjectContext = managedObjectContext
+        self.coreDataStorage = coreDataStorage
+        self.authResponse = authResponse
+        self.result = result
+        
+        super.init()
+    }
+    
+    override func main() {
+        
+        let newAuthResponse = CDAuthResponseEntity.init(authResponse: self.authResponse,
+                                                        insertIntoManagedObjectContext: self.managedObjectContext)
+        
+        self.coreDataStorage.save(accessToken: newAuthResponse.accessToken!) { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let createdWord):
+                    self?.result?(.success(createdWord))
+                    self?.finish()
+                case .failure(let error):
+                    self?.result?(.failure(error))
+                    self?.finish()
+                }
+            }
+        }
+        
+    }
+    
+    deinit {
+        debugPrint(#function, Self.self)
+        self.finish()
+    }
+    
+}
--- MyDictionary_App_Swift/Core/Storage/JWT/MDJWTCoreDataStorage.swift
+++ MyDictionary_App_Swift/Core/Storage/JWT/MDJWTCoreDataStorage.swift
@@ -6,3 +6,131 @@
 //
 
 import Foundation
+import CoreData
+
+protocol MDJWTCoreDataStorageProtocol: MDCRUDJWTProtocol,
+                                       MDEntitiesCountProtocol {
+    
+}
+
+final class MDJWTCoreDataStorage: NSObject,
+                                  MDJWTCoreDataStorageProtocol {
+    
+    fileprivate let operationQueueService: OperationQueueServiceProtocol
+    fileprivate let managedObjectContext: NSManagedObjectContext
+    fileprivate let coreDataStack: CoreDataStack
+    
+    init(operationQueueService: OperationQueueServiceProtocol,
+         managedObjectContext: NSManagedObjectContext,
+         coreDataStack: CoreDataStack) {
+        
+        self.operationQueueService = operationQueueService
+        self.managedObjectContext = managedObjectContext
+        self.coreDataStack = coreDataStack
+        
+    }
+    
+    deinit {
+        debugPrint(#function, Self.self)
+    }
+    
+}
+
+// MARK: - Count
+extension MDJWTCoreDataStorage {
+    
+    func entitiesCount(_ completionHandler: @escaping (MDEntityCountResult)) {
+        
+    }
+    
+}
+
+// MARK: - Create
+extension MDJWTCoreDataStorage {
+    
+    func createJWT(_ authResponse: AuthResponse, _ completionHandler: @escaping (MDEntityResult<AuthResponse>)) {
+        let operation = MDCreateJWTCoreDataStorageOperation.init(managedObjectContext: self.managedObjectContext,
+                                                                 coreDataStorage: self,
+                                                                 authResponse: authResponse) { result in
+            completionHandler(result)
+        }
+        operationQueueService.enqueue(operation)
+    }
+    
+}
+
+// MARK: - Read
+extension MDJWTCoreDataStorage {
+    
+    func readJWT(fromAccessToken accessToken: String, _ completionHandler: @escaping (MDEntityResult<AuthResponse>)) {
+        
+    }
+    
+    func readAllJWTs(fromAccessToken accessToken: String, _ completionHandler: @escaping (MDEntitiesResult<AuthResponse>)) {
+        
+    }
+    
+}
+
+// MARK: - Update
+extension MDJWTCoreDataStorage {
+    
+    func updateJWT(oldAccessToken accessToken: String, newAuthResponse authResponse: AuthResponse, _ completionHandler: @escaping (MDEntityResult<AuthResponse>)) {
+        
+    }
+    
+}
+
+// MARK: - Delete
+extension MDJWTCoreDataStorage {
+    
+    func deleteJWT(_ authResponse: AuthResponse, _ completionHandler: @escaping (MDEntityResult<AuthResponse>)) {
+        
+    }
+    
+}
+
+// MARK: - Save
+extension MDJWTCoreDataStorage {
+    
+    func savePerform(completionHandler: @escaping CDResultSaved) {
+        coreDataStack.savePerform(completionHandler: completionHandler)
+    }
+    
+    func savePerform(accessToken: String, completionHandler: @escaping MDEntityResult<AuthResponse>) {
+        coreDataStack.savePerform() { [unowned self] (result) in
+            switch result {
+            case .success:
+                self.readJWT(fromAccessToken: accessToken) { [unowned self] (result) in
+                    switch result {
+                    case .success(let userEntity):
+                        completionHandler(.success(userEntity))
+                    case .failure(let error):
+                        completionHandler(.failure(error))
+                    }
+                }
+            case .failure(let error):
+                completionHandler(.failure(error))
+            }
+        }
+    }
+    
+    func save(accessToken: String, completionHandler: @escaping MDEntityResult<AuthResponse>) {
+        coreDataStack.savePerformAndWait() { [unowned self] (result) in
+            switch result {
+            case .success:
+                self.readJWT(fromAccessToken: accessToken) { [unowned self] (result) in
+                    switch result {
+                    case .success(let userEntity):
+                        completionHandler(.success(userEntity))
+                    case .failure(let error):
+                        completionHandler(.failure(error))
+                    }
+                }
+            case .failure(let error):
+                completionHandler(.failure(error))
+            }
+        }
+    }
+    
+}
