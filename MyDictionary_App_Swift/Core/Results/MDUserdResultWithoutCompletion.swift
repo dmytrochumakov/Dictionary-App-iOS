17973aa1d53543c86c267439d62648645dc52bb0
Dima <dima.chumakovwork@gmail.com>
2021-08-19 14:07:10 +0300

    edited MDUserStorage

---
+++ MyDictionary_App_Swift/Core/Results/MDUserdResultWithoutCompletion.swift
@@ -0,0 +1,10 @@
+//
+//  MDUserdResultWithoutCompletion.swift
+//  MyDictionary_App_Swift
+//
+//  Created by Dmytro Chumakov on 19.08.2021.
+//
+
+import Foundation
+
+typealias MDUserdResultWithoutCompletion = (Result<UserEntity, Error>)
--- MyDictionary_App_Swift/Core/Storage/User/MDUserStorage.swift
+++ MyDictionary_App_Swift/Core/Storage/User/MDUserStorage.swift
@@ -8,10 +8,22 @@
 import Foundation
 
 protocol MDUserStorageProtocol {
-    func entitiesIsEmpty(storageType: MDStorageType, _ completionHandler: @escaping (MDEntitiesIsEmptyResultWithCompletion))
-    func createUser(_ userEntity: UserEntity, storageType: MDStorageType, _ completionHandler: @escaping(MDEntityResult<UserEntity>))
-    func readUser(fromUserID userId: Int64, storageType: MDStorageType, _ completionHandler: @escaping(MDEntityResult<UserEntity>))
-    func deleteUser(_ userEntity: UserEntity, storageType: MDStorageType,_ completionHandler: @escaping(MDEntityResult<UserEntity>))
+    
+    func entitiesIsEmpty(storageType: MDStorageType,
+                         _ completionHandler: @escaping(MDStorageResultsWithCompletion<MDEntitiesIsEmptyResultWithoutCompletion>))
+    
+    func createUser(_ userEntity: UserEntity,
+                    storageType: MDStorageType,
+                    _ completionHandler: @escaping(MDStorageResultsWithCompletion<MDUserdResultWithoutCompletion>))
+    
+    func readUser(fromUserID userId: Int64,
+                  storageType: MDStorageType,
+                  _ completionHandler: @escaping(MDStorageResultsWithCompletion<MDUserdResultWithoutCompletion>))
+    
+    func deleteUser(_ userEntity: UserEntity,
+                    storageType: MDStorageType,
+                    _ completionHandler: @escaping(MDStorageResultsWithCompletion<MDUserdResultWithoutCompletion>))
+    
 }
 
 final class MDUserStorage: MDUserStorageProtocol {
@@ -36,15 +48,50 @@ final class MDUserStorage: MDUserStorageProtocol {
 // MARK: - Is Empty
 extension MDUserStorage {
     
-    func entitiesIsEmpty(storageType: MDStorageType, _ completionHandler: @escaping (MDEntitiesIsEmptyResultWithCompletion)) {
+    func entitiesIsEmpty(storageType: MDStorageType,
+                         _ completionHandler: @escaping (MDStorageResultsWithCompletion<MDEntitiesIsEmptyResultWithoutCompletion>)) {
+        
         switch storageType {
-        case .none:
-            break
+        
         case .memory:
-            memoryStorage.entitiesIsEmpty(completionHandler)
+            
+            memoryStorage.entitiesIsEmpty() { [unowned self] (result) in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
         case .coreData:
-            coreDataStorage.entitiesIsEmpty(completionHandler)
+            
+            coreDataStorage.entitiesIsEmpty() { [unowned self] (result) in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
+        case .all:
+            
+            // Initialize final result
+            var finalResult: MDStorageResultsWithoutCompletion<MDEntitiesIsEmptyResultWithoutCompletion> = []
+            // Check in Memory
+            memoryStorage.entitiesIsEmpty() { [unowned self] result in
+                
+                finalResult.append(.init(storageType: .memory, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            // Check in Core Data
+            coreDataStorage.entitiesIsEmpty() { [unowned self] result in
+                
+                finalResult.append(.init(storageType: .coreData, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            
         }
+        
     }
     
 }
@@ -52,37 +99,138 @@ extension MDUserStorage {
 // MARK: - CRUD
 extension MDUserStorage {
     
-    func createUser(_ userEntity: UserEntity, storageType: MDStorageType, _ completionHandler: @escaping (MDEntityResult<UserEntity>)) {
+    func createUser(_ userEntity: UserEntity, storageType: MDStorageType, _ completionHandler: @escaping (MDStorageResultsWithCompletion<MDUserdResultWithoutCompletion>)) {
+        
         switch storageType {
-        case .none:
-            break
+        
         case .memory:
-            memoryStorage.createUser(userEntity, completionHandler)
+            
+            memoryStorage.createUser(userEntity) { [unowned self] (result) in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
         case .coreData:
-            coreDataStorage.createUser(userEntity, completionHandler)
+            
+            coreDataStorage.createUser(userEntity) { [unowned self] (result) in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
+        case .all:
+            
+            // Initialize final result
+            var finalResult: MDStorageResultsWithoutCompletion<MDUserdResultWithoutCompletion> = []
+            // Create in Memory
+            memoryStorage.createUser(userEntity) { [unowned self] result in
+                
+                finalResult.append(.init(storageType: .memory, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            // Create in Core Data
+            coreDataStorage.createUser(userEntity) { [unowned self] result in
+                
+                finalResult.append(.init(storageType: .coreData, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            
         }
     }
     
-    func readUser(fromUserID userId: Int64, storageType: MDStorageType, _ completionHandler: @escaping (MDEntityResult<UserEntity>)) {
+    func readUser(fromUserID userId: Int64, storageType: MDStorageType, _ completionHandler: @escaping (MDStorageResultsWithCompletion<MDUserdResultWithoutCompletion>)) {
+        
         switch storageType {
-        case .none:
-            break
+        
         case .memory:
-            memoryStorage.readUser(fromUserID: userId, completionHandler)
+            
+            memoryStorage.readUser(fromUserID: userId) { [unowned self] (result) in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
         case .coreData:
-            coreDataStorage.readUser(fromUserID: userId, completionHandler)
+            
+            coreDataStorage.readUser(fromUserID: userId) { [unowned self] (result) in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
+        case .all:
+            
+            // Initialize final result
+            var finalResult: MDStorageResultsWithoutCompletion<MDUserdResultWithoutCompletion> = []
+            // Read From Memory
+            memoryStorage.readUser(fromUserID: userId) { [unowned self] result in
+                
+                finalResult.append(.init(storageType: .memory, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            // Read From Core Data
+            coreDataStorage.readUser(fromUserID: userId) { [unowned self] result in
+                
+                finalResult.append(.init(storageType: .coreData, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            
         }
+        
     }
     
-    func deleteUser(_ userEntity: UserEntity, storageType: MDStorageType, _ completionHandler: @escaping (MDEntityResult<UserEntity>)) {
+    func deleteUser(_ userEntity: UserEntity, storageType: MDStorageType, _ completionHandler: @escaping (MDStorageResultsWithCompletion<MDUserdResultWithoutCompletion>)) {
+        
         switch storageType {
-        case .none:
-            break
+        
         case .memory:
-            memoryStorage.deleteUser(userEntity, completionHandler)
+            
+            memoryStorage.deleteUser(userEntity) { [unowned self] (result) in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
         case .coreData:
-            coreDataStorage.deleteUser(userEntity, completionHandler)
+            
+            coreDataStorage.deleteUser(userEntity) { [unowned self] (result) in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
+        case .all:
+            
+            // Initialize final result
+            var finalResult: MDStorageResultsWithoutCompletion<MDUserdResultWithoutCompletion> = []
+            // Delete From Memory
+            memoryStorage.deleteUser(userEntity) { [unowned self] result in
+                
+                finalResult.append(.init(storageType: .memory, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            // Delete From Core Data
+            coreDataStorage.deleteUser(userEntity) { [unowned self] result in
+                
+                finalResult.append(.init(storageType: .coreData, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            
         }
+        
     }
     
 }
