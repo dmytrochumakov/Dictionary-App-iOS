8ded3ee36337a4ba1a38c9bdeeafc20473327ead
Dima <dima.chumakovwork@gmail.com>
2021-08-19 14:23:24 +0300

    edited MDJWTStorage

--- MyDictionary_App_Swift/Core/Storage/JWT/MDJWTStorage.swift
+++ MyDictionary_App_Swift/Core/Storage/JWT/MDJWTStorage.swift
@@ -9,20 +9,29 @@ import Foundation
 
 protocol MDJWTStorageProtocol {
     
-    func entitiesIsEmpty(storageType: MDStorageType, _ completionHandler: @escaping (MDEntitiesIsEmptyResultWithCompletion))
+    func entitiesIsEmpty(storageType: MDStorageType,
+                         _ completionHandler: @escaping (MDStorageResultsWithCompletion<MDEntitiesIsEmptyResultWithoutCompletion>))
     
-    func createJWT(storageType: MDStorageType, jwtResponse: JWTResponse, _ completionHandler: @escaping(MDEntityResult<JWTResponse>))
+    func createJWT(storageType: MDStorageType,
+                   jwtResponse: JWTResponse,
+                   _ completionHandler: @escaping(MDStorageResultsWithCompletion<MDJWTResultWithoutCompletion>))
     
-    func readJWT(storageType: MDStorageType, fromAccessToken accessToken: String, _ completionHandler: @escaping(MDEntityResult<JWTResponse>))
+    func readJWT(storageType: MDStorageType,
+                 fromAccessToken accessToken: String,
+                 _ completionHandler: @escaping(MDStorageResultsWithCompletion<MDJWTResultWithoutCompletion>))
     
     func updateJWT(storageType: MDStorageType,
                    oldAccessToken accessToken: String,
                    newJWTResponse jwtResponse: JWTResponse,
-                   _ completionHandler: @escaping(MDEntityResult<JWTResponse>))
+                   _ completionHandler: @escaping(MDStorageResultsWithCompletion<MDJWTResultWithoutCompletion>))
     
-    func deleteJWT(storageType: MDStorageType, jwtResponse: JWTResponse, _ completionHandler: @escaping(MDEntityResult<JWTResponse>))
+    func deleteJWT(storageType: MDStorageType,
+                   jwtResponse: JWTResponse,
+                   _ completionHandler: @escaping(MDStorageResultsWithCompletion<MDJWTResultWithoutCompletion>))
     
 }
+
+typealias MDJWTResultWithoutCompletion = (Result<JWTResponse, Error>)
 
 final class MDJWTStorage: MDJWTStorageProtocol {
     
@@ -46,15 +55,50 @@ final class MDJWTStorage: MDJWTStorageProtocol {
 // MARK: - Is Empty
 extension MDJWTStorage {
     
-    func entitiesIsEmpty(storageType: MDStorageType, _ completionHandler: @escaping (MDEntitiesIsEmptyResultWithCompletion)) {
+    func entitiesIsEmpty(storageType: MDStorageType,
+                         _ completionHandler: @escaping (MDStorageResultsWithCompletion<MDEntitiesIsEmptyResultWithoutCompletion>)) {
+        
         switch storageType {
-        case .none:
-            break
+        
         case .memory:
-            memoryStorage.entitiesIsEmpty(completionHandler)
+            
+            memoryStorage.entitiesIsEmpty() { [unowned self] (result) in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
         case .coreData:
-            coreDataStorage.entitiesIsEmpty(completionHandler)
+            
+            coreDataStorage.entitiesIsEmpty() { [unowned self] (result) in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
+        case .all:
+            
+            // Initialize final result
+            var finalResult: MDStorageResultsWithoutCompletion<MDEntitiesIsEmptyResultWithoutCompletion> = []
+            // Check in Memory
+            memoryStorage.entitiesIsEmpty() { [unowned self] result in
+                
+                finalResult.append(.init(storageType: .memory, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            // Check in Core Data
+            coreDataStorage.entitiesIsEmpty() { [unowned self] result in
+                
+                finalResult.append(.init(storageType: .coreData, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            
         }
+        
     }
     
 }
@@ -62,48 +106,196 @@ extension MDJWTStorage {
 // MARK: - CRUD
 extension MDJWTStorage {
     
-    func createJWT(storageType: MDStorageType, jwtResponse: JWTResponse, _ completionHandler: @escaping (MDEntityResult<JWTResponse>)) {
+    func createJWT(storageType: MDStorageType,
+                   jwtResponse: JWTResponse,
+                   _ completionHandler: @escaping (MDStorageResultsWithCompletion<MDJWTResultWithoutCompletion>)) {
+        
         switch storageType {
-        case .none:
-            break
+        
         case .memory:
-            memoryStorage.createJWT(jwtResponse, completionHandler)
+            
+            memoryStorage.createJWT(jwtResponse) { [unowned self] (result) in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
         case .coreData:
-            coreDataStorage.createJWT(jwtResponse, completionHandler)
+            
+            coreDataStorage.createJWT(jwtResponse) { [unowned self] (result) in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
+        case .all:
+            
+            // Initialize final result
+            var finalResult: MDStorageResultsWithoutCompletion<MDJWTResultWithoutCompletion> = []
+            // Create in Memory
+            memoryStorage.createJWT(jwtResponse) { [unowned self] result in
+                
+                finalResult.append(.init(storageType: .memory, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            // Create in Core Data
+            coreDataStorage.createJWT(jwtResponse) { [unowned self] result in
+                
+                finalResult.append(.init(storageType: .coreData, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            
         }
+        
     }
     
-    func readJWT(storageType: MDStorageType, fromAccessToken accessToken: String, _ completionHandler: @escaping (MDEntityResult<JWTResponse>)) {
+    func readJWT(storageType: MDStorageType,
+                 fromAccessToken accessToken: String,
+                 _ completionHandler: @escaping (MDStorageResultsWithCompletion<MDJWTResultWithoutCompletion>)) {
+        
         switch storageType {
-        case .none:
-            break
+        
         case .memory:
-            memoryStorage.readJWT(fromAccessToken: accessToken, completionHandler)
+            
+            memoryStorage.readJWT(fromAccessToken: accessToken) { [unowned self] (result) in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
         case .coreData:
-            coreDataStorage.readJWT(fromAccessToken: accessToken, completionHandler)
+            
+            coreDataStorage.readJWT(fromAccessToken: accessToken) { [unowned self] (result) in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
+        case .all:
+            
+            // Initialize final result
+            var finalResult: MDStorageResultsWithoutCompletion<MDJWTResultWithoutCompletion> = []
+            // Read From Memory
+            memoryStorage.readJWT(fromAccessToken: accessToken) { [unowned self] result in
+                
+                finalResult.append(.init(storageType: .memory, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            // Read From Core Data
+            coreDataStorage.readJWT(fromAccessToken: accessToken) { [unowned self] (result) in
+                
+                finalResult.append(.init(storageType: .coreData, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            
         }
+        
     }
     
-    func updateJWT(storageType: MDStorageType, oldAccessToken accessToken: String, newJWTResponse jwtResponse: JWTResponse, _ completionHandler: @escaping (MDEntityResult<JWTResponse>)) {
+    func updateJWT(storageType: MDStorageType,
+                   oldAccessToken accessToken: String,
+                   newJWTResponse jwtResponse: JWTResponse,
+                   _ completionHandler: @escaping (MDStorageResultsWithCompletion<MDJWTResultWithoutCompletion>)) {
+        
         switch storageType {
-        case .none:
-            break
+        
         case .memory:
-            memoryStorage.updateJWT(oldAccessToken: accessToken, newJWTResponse: jwtResponse, completionHandler)
+            
+            memoryStorage.updateJWT(oldAccessToken: accessToken,
+                                    newJWTResponse: jwtResponse) { [unowned self] result in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
         case .coreData:
-            coreDataStorage.updateJWT(oldAccessToken: accessToken, newJWTResponse: jwtResponse, completionHandler)
+            
+            coreDataStorage.updateJWT(oldAccessToken: accessToken, newJWTResponse: jwtResponse) { [unowned self] (result) in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
+        case .all:
+            
+            // Initialize final result
+            var finalResult: MDStorageResultsWithoutCompletion<MDJWTResultWithoutCompletion> = []
+            // Update In Memory
+            memoryStorage.updateJWT(oldAccessToken: accessToken,
+                                    newJWTResponse: jwtResponse) { [unowned self] result in
+                
+                finalResult.append(.init(storageType: .memory, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            // Update In Core Data
+            coreDataStorage.updateJWT(oldAccessToken: accessToken,
+                                      newJWTResponse: jwtResponse) { [unowned self] (result) in
+                
+                finalResult.append(.init(storageType: .coreData, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            
         }
+        
     }
     
-    func deleteJWT(storageType: MDStorageType, jwtResponse: JWTResponse, _ completionHandler: @escaping (MDEntityResult<JWTResponse>)) {
+    func deleteJWT(storageType: MDStorageType,
+                   jwtResponse: JWTResponse,
+                   _ completionHandler: @escaping (MDStorageResultsWithCompletion<MDJWTResultWithoutCompletion>)) {
+        
         switch storageType {
-        case .none:
-            break
+        
         case .memory:
-            memoryStorage.deleteJWT(jwtResponse, completionHandler)
+            
+            memoryStorage.deleteJWT(jwtResponse) { [unowned self] result in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
         case .coreData:
-            coreDataStorage.deleteJWT(jwtResponse, completionHandler)
+            
+            coreDataStorage.deleteJWT(jwtResponse) { [unowned self] result in
+                completionHandler([.init(storageType: storageType, result: result)])
+            }
+            
+        case .all:
+            
+            // Initialize final result
+            var finalResult: MDStorageResultsWithoutCompletion<MDJWTResultWithoutCompletion> = []
+            // Delete From Memory
+            memoryStorage.deleteJWT(jwtResponse) { [unowned self] result in
+                
+                finalResult.append(.init(storageType: .memory, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            // Delete From Core Data
+            coreDataStorage.deleteJWT(jwtResponse) { [unowned self] result in
+                
+                finalResult.append(.init(storageType: .coreData, result: result))
+                
+                if (finalResult.count == MDStorageType.allCases.count) {
+                    completionHandler(finalResult)
+                }
+                
+            }
+            
         }
+        
     }
     
 }
