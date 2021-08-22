6ec321a97c17c57a1826c1cf045ccaa3eee0dcb9
Dima <dima.chumakovwork@gmail.com>
2021-08-22 14:03:37 +0300

    added CDCourseEntityAttributeName

---
+++ MyDictionary_App_Swift/CoreData/AttributeName/CDCourseEntityAttributeName.swift
@@ -0,0 +1,17 @@
+//
+//  CDCourseEntityAttributeName.swift
+//  MyDictionary_App_Swift
+//
+//  Created by Dmytro Chumakov on 22.08.2021.
+//
+
+import Foundation
+
+struct CDCourseEntityAttributeName {
+    static let userId: String = "userId"
+    static let courseId: String = "courseId"
+    static let languageId = "languageId"
+    static let languageName: String = "languageName"
+    static let createdAt: String = "createdAt"
+    static let updatedAt: String = "updatedAt"
+}
