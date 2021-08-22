ffcadfdf40c57101d6d1a538911ca749a89eaf4b
Dima <dima.chumakovwork@gmail.com>
2021-08-22 13:34:54 +0300

    added LanguageEntity

---
+++ MyDictionary_App_Swift/Core/Network/Response/LanguageEntity.swift
@@ -0,0 +1,34 @@
+//
+//  LanguageEntity.swift
+//  MyDictionary_App_Swift
+//
+//  Created by Dmytro Chumakov on 22.08.2021.
+//
+
+import Foundation
+
+struct LanguageEntity {
+    
+    let languageId: Int64
+    let languageName: String
+    let createdAt: String
+    
+}
+
+// MARK: - Decodable
+extension LanguageEntity: Decodable {
+    
+    enum CodingKeys: String, CodingKey {
+        case languageId = "language_id"
+        case languageName = "language_name"
+        case createdAt = "created_at"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.languageId = try container.decode(Int64.self, forKey: .languageId)
+        self.languageName = try container.decode(String.self, forKey: .languageName)
+        self.createdAt = try container.decode(String.self, forKey: .createdAt)
+    }
+    
+}
