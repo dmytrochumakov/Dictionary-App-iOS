3792d506c64b5e0a116926376eb3dbd6f7c2c006
Dima <dima.chumakovwork@gmail.com>
2021-08-23 10:15:01 +0300

    added language storages

---
+++ MyDictionary_App_Swift/Core/Storage/Language/MDLanguageCoreDataStorage.swift
@@ -0,0 +1,16 @@
+//
+//  MDLanguageCoreDataStorage.swift
+//  MyDictionary_App_Swift
+//
+//  Created by Dmytro Chumakov on 23.08.2021.
+//
+
+import Foundation
+
+protocol MDLanguageCoreDataStorageProtocol {
+    
+}
+
+final class MDLanguageCoreDataStorage: MDLanguageCoreDataStorageProtocol {
+    
+}
---
+++ MyDictionary_App_Swift/Core/Storage/Language/MDLanguageMemoryStorage.swift
@@ -0,0 +1,16 @@
+//
+//  MDLanguageMemoryStorage.swift
+//  MyDictionary_App_Swift
+//
+//  Created by Dmytro Chumakov on 23.08.2021.
+//
+
+import Foundation
+
+protocol MDLanguageMemoryStorageProtocol {
+    
+}
+
+final class MDLanguageMemoryStorage: MDLanguageMemoryStorageProtocol {
+    
+}
