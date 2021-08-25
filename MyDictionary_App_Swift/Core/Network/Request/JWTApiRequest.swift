4b441ccf089bf724207fee41a0e13c6381e49006
Dima <dima.chumakovwork@gmail.com>
2021-08-25 15:55:28 +0300

    added JWTApiRequest

---
+++ MyDictionary_App_Swift/Core/Network/Request/JWTApiRequest.swift
@@ -0,0 +1,37 @@
+//
+//  JWTApiRequest.swift
+//  MyDictionary_App_Swift
+//
+//  Created by Dmytro Chumakov on 25.08.2021.
+//
+
+import Foundation
+
+struct JWTApiRequest {
+    
+    let nickname: String
+    let password: String
+    let userId: Int64
+    let oldJWT: String
+    
+}
+
+// MARK: - Encodable
+extension JWTApiRequest: Encodable {
+    
+    enum CodingKeys: String, CodingKey {
+        case nickname = "nickname"
+        case password = "password"
+        case userId = "user_id"
+        case oldJWT = "old_jwt"
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(nickname, forKey: .nickname)
+        try container.encode(password, forKey: .password)
+        try container.encode(userId, forKey: .userId)
+        try container.encode(oldJWT, forKey: .oldJWT)
+    }
+    
+}
