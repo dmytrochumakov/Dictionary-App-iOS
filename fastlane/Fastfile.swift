e1d85d8c87c8ec424d275948fd59b5e441d56f29
Dima <dima.chumakovwork@gmail.com>
2021-09-05 11:02:56 +0300

    edited fastfile && gemfile

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -9,8 +9,9 @@
 import Foundation
 
 class Fastfile: LaneFile {
-	func customLane() {
-	desc("Description of what the lane does")
-		// add actions here: https://docs.fastlane.tools/actions
+    
+	func unitTests() {
+        desc("Description of what the lane does")
 	}
+    
 }
