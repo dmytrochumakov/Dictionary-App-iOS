00f2a9faef64db134d327ee4f2c66570deeb271a
Dima <dima.chumakovwork@gmail.com>
2021-09-06 12:00:14 +0300

    edited test_app.sh

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 xcodebuild -workspace MyDictionary_App_Swift.xcworkspace \
             -scheme MyDictionary_App_Swift \
-            -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
+            -destination platform=iOS\ Simulator,OS=14.4,name=iPhone\ 11 \
             clean test | xcpretty
