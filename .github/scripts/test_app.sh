e834feb7b72ccd803f051d10a386aa89397bf562
Dima <dima.chumakovwork@gmail.com>
2021-09-06 14:31:14 +0300

    deleted scripts folder

--- .github/scripts/test_app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -workspace MyDictionary_App_Swift.xcworkspace \
-            -scheme MyDictionary_App_Swift \
-            -destination platform=iOS\ Simulator,OS=14.5,name=iPhone\ 11 \
-            clean test | xcpretty
