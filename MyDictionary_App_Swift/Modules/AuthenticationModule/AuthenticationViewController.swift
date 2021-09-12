502e70398dfa4cfb75fd6f083bf43f556f41deec
Dima <dima.chumakovwork@gmail.com>
2021-09-12 11:18:48 +0300

    added  backButton, loginlabel to AuthenticationViewController

--- MyDictionary_App_Swift/Modules/AuthenticationModule/AuthenticationViewController.swift
+++ MyDictionary_App_Swift/Modules/AuthenticationModule/AuthenticationViewController.swift
@@ -9,7 +9,24 @@ import UIKit
 final class AuthenticationViewController: BaseAuthViewController {
     
     fileprivate let presenter: AuthenticationPresenterInputProtocol
-        
+    
+    fileprivate let backButtonSize: CGSize = .init(width: 40, height: 40)
+    fileprivate let backButton: UIButton = {
+        let button: UIButton = .init()
+        button.setImage(MDAppStyling.Image.back_white_arrow.image, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    fileprivate let loginLabel: UILabel = {
+        let label: UILabel = .init()
+        label.font = MDAppStyling.Font.MyriadProSemiBold.font(ofSize: 34)
+        label.text = KeysForTranslate.login.localized
+        label.textColor = MDAppStyling.Color.md_White_0_Light_Appearence.color()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     init(presenter: AuthenticationPresenterInputProtocol) {
         self.presenter = presenter
         super.init()
@@ -64,7 +81,17 @@ extension AuthenticationViewController: AuthenticationPresenterOutputProtocol {
 fileprivate extension AuthenticationViewController {
     
     func addViews() {
-        
+        addLoginLabel()
+        addBackButton()
+    }
+    
+    func addLoginLabel() {
+        view.addSubview(loginLabel)
+    }
+    
+    func addBackButton() {
+        backButton.addTarget(self, action: #selector(backButtonAction), for: .touchUpInside)
+        view.addSubview(backButton)
     }
     
 }
@@ -73,6 +100,37 @@ fileprivate extension AuthenticationViewController {
 fileprivate extension AuthenticationViewController {
     
     func addConstraints() {
+        addLoginLabelConstraints()
+        addBackButtonConstraints()
+    }
+    
+    func addLoginLabelConstraints() {
+        
+        NSLayoutConstraint.addEqualLeftConstraintAndActivate(item: self.loginLabel,
+                                                             toItem: self.navigationBarView,
+                                                             constant: 16)
+        
+        NSLayoutConstraint.addEqualRightConstraintAndActivate(item: self.loginLabel,
+                                                              toItem: self.navigationBarView,
+                                                              constant: -16)
+        
+        NSLayoutConstraint.addEqualBottomConstraintAndActivate(item: self.loginLabel,
+                                                               toItem: self.navigationBarView,
+                                                               constant: -16)
+        
+    }
+    
+    func addBackButtonConstraints() {
+        
+        NSLayoutConstraint.addEqualConstraintAndActivate(item: self.backButton,
+                                                         attribute: .bottom,
+                                                         toItem: self.loginLabel,
+                                                         attribute: .top,
+                                                         constant: -16)
+        
+        NSLayoutConstraint.addEqualLeftConstraintAndActivate(item: self.backButton,
+                                                             toItem: self.navigationBarView,
+                                                             constant: 18)
         
     }
     
@@ -90,6 +148,9 @@ fileprivate extension AuthenticationViewController {
 // MARK: - Actions
 fileprivate extension AuthenticationViewController {
     
+    @objc func backButtonAction() {
+        navigationController?.popViewController(animated: true)
+    }
     
 }
 
