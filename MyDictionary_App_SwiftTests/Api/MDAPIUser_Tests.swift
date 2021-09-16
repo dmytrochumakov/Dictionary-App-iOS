22b3d727562900c71ae9a3af54d703ae7214cda7
Dima <dima.chumakovwork@gmail.com>
2021-09-16 16:57:09 +0300

    added MDAPIUser_Tests

---
+++ MyDictionary_App_SwiftTests/Api/MDAPIUser_Tests.swift
@@ -0,0 +1,76 @@
+//
+//  MDAPIUser_Tests.swift
+//  MyDictionary_App_SwiftTests
+//
+//  Created by Dmytro Chumakov on 16.09.2021.
+//
+
+import XCTest
+@testable import MyDictionary_App_Swift
+
+final class MDAPIUser_Tests: XCTestCase {
+    
+    fileprivate var apiJWT: MDAPIJWTProtocol!
+    fileprivate var apiUser: MDAPIUserProtocol!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        let requestDispatcher: MDRequestDispatcherProtocol = MDConstants.RequestDispatcher.defaultRequestDispatcher(reachability: try! .init())
+        
+        let operationQueue: OperationQueue = .init()
+        
+        let operationQueueService: OperationQueueServiceProtocol = OperationQueueService.init(operationQueue: operationQueue)
+        
+        self.apiUser = MDAPIUser.init(requestDispatcher: requestDispatcher,
+                                      operationQueueService: operationQueueService)
+        
+        self.apiJWT = MDAPIJWT.init(requestDispatcher: requestDispatcher,
+                                    operationQueueService: operationQueueService)
+        
+    }
+    
+}
+
+extension MDAPIUser_Tests {
+    
+    func test_Login() {
+        
+        let expectation = XCTestExpectation(description: "Login Expectation")
+        
+        apiJWT.accessToken(jwtApiRequest: Constants_For_Tests.jwtApiRequest) { [unowned self] jwtResult in
+            
+            switch jwtResult {
+            
+            case .success(let jwtResponse):
+                
+                apiUser.getUser(accessToken: jwtResponse.accessToken,
+                                byUserId: Constants_For_Tests.jwtApiRequest.userId) { userResult in
+                    
+                    switch userResult {
+                    
+                    case .success:
+                        
+                        expectation.fulfill()
+                        
+                    case .failure(let error):
+                        XCTExpectFailure(error.localizedDescription)
+                        expectation.fulfill()
+                    }
+                    
+                }
+                
+                
+            case .failure(let error):
+                XCTExpectFailure(error.localizedDescription)
+                expectation.fulfill()
+            }
+            
+        }
+        
+        wait(for: [expectation], timeout: Constants_For_Tests.testExpectationTimeout)
+        
+    }
+    
+}
+
