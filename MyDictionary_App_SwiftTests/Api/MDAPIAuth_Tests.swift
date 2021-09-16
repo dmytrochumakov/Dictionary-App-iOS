45f50eb8aaa91d3d44b78480887a8e81558694fe
Dima <dima.chumakovwork@gmail.com>
2021-09-16 16:50:25 +0300

    added MDAPIAuth_Tests

---
+++ MyDictionary_App_SwiftTests/Api/MDAPIAuth_Tests.swift
@@ -0,0 +1,56 @@
+//
+//  MDAPIAuth_Tests.swift
+//  MyDictionary_App_SwiftTests
+//
+//  Created by Dmytro Chumakov on 16.09.2021.
+//
+
+import XCTest
+@testable import MyDictionary_App_Swift
+
+final class MDAPIAuth_Tests: XCTestCase {
+    
+    fileprivate var apiAuth: MDAPIAuthProtocol!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        let requestDispatcher: MDRequestDispatcherProtocol = MDConstants.RequestDispatcher.defaultRequestDispatcher(reachability: try! .init())
+        
+        let operationQueue: OperationQueue = .init()
+        
+        let operationQueueService: OperationQueueServiceProtocol = OperationQueueService.init(operationQueue: operationQueue)
+        
+        self.apiAuth = MDAPIAuth.init(requestDispatcher: requestDispatcher,
+                                      operationQueueService: operationQueueService)
+        
+    }
+    
+}
+
+extension MDAPIAuth_Tests {
+    
+    func test_Login() {
+        
+        let expectation = XCTestExpectation(description: "Login Expectation")
+        
+        apiAuth.login(authRequest: Constants_For_Tests.authRequest) { authResult in
+            
+            switch authResult {
+            
+            case .success:
+                
+                expectation.fulfill()
+                
+            case .failure(let error):
+                XCTExpectFailure(error.localizedDescription)
+                expectation.fulfill()
+            }
+            
+        }
+        
+        wait(for: [expectation], timeout: Constants_For_Tests.testExpectationTimeout)
+        
+    }
+    
+}
