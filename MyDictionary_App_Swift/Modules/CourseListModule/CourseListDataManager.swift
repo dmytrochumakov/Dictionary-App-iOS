0d5fccde46056d4f44e577a3fa3234478200fd0e
Dima <dima.chumakovwork@gmail.com>
2021-09-20 12:31:34 +0300

    deleted testdata

--- MyDictionary_App_Swift/Modules/CourseListModule/CourseListDataManager.swift
+++ MyDictionary_App_Swift/Modules/CourseListModule/CourseListDataManager.swift
@@ -24,18 +24,6 @@ protocol CourseListDataManagerProtocol: CourseListDataManagerInputProtocol {
 
 final class CourseListDataManager: CourseListDataManagerProtocol {
     
-    fileprivate let testData: [CourseResponse] = [.init(userId: 1,
-                                                        courseId: 1,
-                                                        languageId: 1,
-                                                        languageName: "English",
-                                                        createdAt: "2021-08-15T10:34:33.998Z"),
-                                                  .init(userId: 1,
-                                                        courseId: 2,
-                                                        languageId: 2,
-                                                        languageName: "Spanish",
-                                                        createdAt: "2021-08-15T10:34:33.998Z")
-    ]
-    
     fileprivate let memoryStorage: MDCourseMemoryStorageProtocol
     internal var dataProvider: CourseListDataProviderProtocol
     internal weak var dataManagerOutput: CourseListDataManagerOutputProtocol?
@@ -64,7 +52,7 @@ extension CourseListDataManager {
             switch readResult {
             
             case .success(let readCourses):
-                self?.dataProvider.filteredCourses = self?.testData ?? []
+                self?.dataProvider.filteredCourses = readCourses
                 completionHandler(.success(()))
             case .failure(let error):
                 completionHandler(.failure(error))
