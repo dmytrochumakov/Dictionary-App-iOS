4810a1a8c982b72b740a5942b7d1d3d026819d32
Dima <dima.chumakovwork@gmail.com>
2021-09-28 14:08:19 +0300

    changed enviroment

--- MyDictionary_App_Swift/Common/Constants/MDConstants.swift
+++ MyDictionary_App_Swift/Common/Constants/MDConstants.swift
@@ -73,7 +73,7 @@ struct MDConstants {
     }
     
     struct Environment {
-        static let current: MDEnvironment = .development
+        static let current: MDEnvironment = .production
     }
     
     struct APIEnvironment {
