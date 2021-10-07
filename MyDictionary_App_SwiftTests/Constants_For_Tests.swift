24427d9dcda51f75c604fb6e3f9f9274446dd80f
Dima <dima.chumakovwork@gmail.com>
2021-10-07 12:45:19 +0300

    updated user id for production test

--- MyDictionary_App_SwiftTests/Constants_For_Tests.swift
+++ MyDictionary_App_SwiftTests/Constants_For_Tests.swift
@@ -79,7 +79,7 @@ struct Constants_For_Tests {
         case .production:
             return .init(nickname: authRequest.nickname,
                          password: authRequest.password,
-                         userId: 1)
+                         userId: 6)
         }
         
     }
