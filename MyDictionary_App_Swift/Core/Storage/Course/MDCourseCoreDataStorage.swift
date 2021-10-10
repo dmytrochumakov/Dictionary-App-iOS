64502275084dd85998b2a9b5307593743e44944c
Dima <dima.chumakovwork@gmail.com>
2021-10-10 12:17:43 +0300

    Transfer course core data operations to the storage and delete unnecessary files

--- MyDictionary_App_Swift/Core/Storage/Course/MDCourseCoreDataStorage.swift
+++ MyDictionary_App_Swift/Core/Storage/Course/MDCourseCoreDataStorage.swift
@@ -68,11 +68,39 @@ extension MDCourseCoreDataStorage {
     func createCourse(_ courseEntity: CourseResponse,
                       _ completionHandler: @escaping (MDOperationResultWithCompletion<CourseResponse>)) {
         
-        let operation: MDCreateCourseCoreDataStorageOperation = .init(managedObjectContext: self.managedObjectContext,
-                                                                      coreDataStack: self.coreDataStack,
-                                                                      coreDataStorage: self,
-                                                                      courseEntity: courseEntity) { result in
-            completionHandler(result)
+        let operation: BlockOperation = .init {
+            
+            let newCourseEntity = CDCourseResponseEntity.init(courseResponse: courseEntity,
+                                                              insertIntoManagedObjectContext: self.managedObjectContext)
+            
+            self.coreDataStack.save(managedObjectContext: self.managedObjectContext) { result in
+                
+                switch result {
+                    
+                case .success:
+                    
+                    //
+                    completionHandler(.success((newCourseEntity.courseResponse)))
+                    //
+                    
+                    //
+                    break
+                    //
+                    
+                case .failure(let error):
+                    
+                    //
+                    completionHandler(.failure(error))
+                    //
+                    
+                    //
+                    break
+                    //
+                    
+                }
+                
+            }
+            
         }
         
         //
@@ -84,11 +112,67 @@ extension MDCourseCoreDataStorage {
     func createCourses(_ courseEntities: [CourseResponse],
                        _ completionHandler: @escaping (MDOperationsResultWithCompletion<CourseResponse>)) {
         
-        let operation: MDCreateCoursesCoreDataStorageOperation = .init(managedObjectContext: self.managedObjectContext,
-                                                                       coreDataStack: self.coreDataStack,
-                                                                       coreDataStorage: self,
-                                                                       courseEntities: courseEntities) { result in
-            completionHandler(result)
+        let operation: BlockOperation = .init {
+            
+            if (courseEntities.isEmpty) {
+                
+                //
+                completionHandler(.success(courseEntities))
+                //
+                
+                //
+                return
+                //
+                
+            } else {
+                
+                var resultCount: Int = .zero
+                
+                courseEntities.forEach { courseEntity in
+                    
+                    let _ = CDCourseResponseEntity.init(courseResponse: courseEntity,
+                                                        insertIntoManagedObjectContext: self.managedObjectContext)
+                    
+                    
+                    self.coreDataStack.save(managedObjectContext: self.managedObjectContext) { result in
+                        
+                        switch result {
+                            
+                        case .success:
+                            
+                            //
+                            resultCount += 1
+                            //
+                            
+                            if (resultCount == courseEntities.count) {
+                                
+                                //
+                                completionHandler(.success(courseEntities))
+                                //
+                                
+                            }
+                            
+                            //
+                            break
+                            //
+                            
+                        case .failure(let error):
+                            
+                            //
+                            completionHandler(.failure(error))
+                            
+                            //
+                            break
+                            //
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
         }
         
         //
@@ -98,17 +182,52 @@ extension MDCourseCoreDataStorage {
     }
     
 }
-    
+
 // MARK: - Read
 extension MDCourseCoreDataStorage {
     
     func readCourse(fromCourseId courseId: Int64,
                     _ completionHandler: @escaping (MDOperationResultWithCompletion<CourseResponse>)) {
         
-        let operation: MDReadCourseCoreDataStorageOperation = .init(managedObjectContext: self.managedObjectContext,
-                                                                    coreDataStorage: self,
-                                                                    courseId: courseId) { result in
-            completionHandler(result)
+        let operation: BlockOperation = .init {
+            
+            let fetchRequest = NSFetchRequest<CDCourseResponseEntity>(entityName: CoreDataEntityName.CDCourseResponseEntity)
+            fetchRequest.predicate = NSPredicate(format: "\(CDCourseResponseEntityAttributeName.courseId) == %i", courseId)
+            
+            do {
+                if let courseEntity = try self.managedObjectContext.fetch(fetchRequest).map({ $0.courseResponse }).first {
+                    
+                    //
+                    completionHandler(.success(courseEntity))
+                    //
+                    
+                    //
+                    return
+                    //
+                    
+                } else {
+                    
+                    //
+                    completionHandler(.failure(MDEntityOperationError.cantFindEntity))
+                    //
+                    
+                    //
+                    return
+                    //
+                    
+                }
+            } catch {
+                
+                //
+                completionHandler(.failure(error))
+                //
+                
+                //
+                return
+                //
+                
+            }
+            
         }
         
         //
@@ -119,9 +238,32 @@ extension MDCourseCoreDataStorage {
     
     func readAllCourses(_ completionHandler: @escaping (MDOperationResultWithCompletion<[CourseResponse]>)) {
         
-        let operation: MDReadAllCoursesCoreDataStorageOperation = .init(managedObjectContext: self.managedObjectContext,
-                                                                        coreDataStorage: self) { result in
-            completionHandler(result)
+        let operation: BlockOperation = .init {
+            
+            let fetchRequest = NSFetchRequest<CDCourseResponseEntity>(entityName: CoreDataEntityName.CDCourseResponseEntity)
+            
+            do {
+                
+                //
+                completionHandler(.success(try self.managedObjectContext.fetch(fetchRequest).map({ $0.courseResponse })))
+                //
+                
+                //
+                return
+                //
+                
+            } catch {
+                
+                //
+                completionHandler(.failure(error))
+                //
+                
+                //
+                return
+                //
+                
+            }
+            
         }
         
         //
@@ -138,11 +280,58 @@ extension MDCourseCoreDataStorage {
     func deleteCourse(fromCourseId courseId: Int64,
                       _ completionHandler: @escaping (MDOperationResultWithCompletion<Void>)) {
         
-        let operation: MDDeleteCourseCoreDataStorageOperation = .init(managedObjectContext: self.managedObjectContext,
-                                                                      coreDataStack: self.coreDataStack,
-                                                                      coreDataStorage: self,
-                                                                      courseId: courseId) { result in
-            completionHandler(result)
+        let operation: BlockOperation = .init {
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: CoreDataEntityName.CDCourseResponseEntity)
+            fetchRequest.predicate = NSPredicate(format: "\(CDCourseResponseEntityAttributeName.courseId) == %i", courseId)
+            
+            let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+            
+            do {
+                
+                try self.managedObjectContext.execute(batchDeleteRequest)
+                
+                self.coreDataStack.save(managedObjectContext: self.managedObjectContext) { result in
+                    
+                    switch result {
+                        
+                    case .success:
+                        
+                        //
+                        completionHandler(.success(()))
+                        //
+                        
+                        //
+                        break
+                        //
+                        
+                    case .failure(let error):
+                        
+                        //
+                        completionHandler(.failure(error))
+                        //
+                        
+                        //
+                        break
+                        //
+                        
+                    }
+                    
+                }
+                
+            } catch {
+                
+                //
+                completionHandler(.failure(error))
+                //
+                
+                //
+                return
+                //
+                
+            }
+            
+            
         }
         
         //
@@ -153,10 +342,57 @@ extension MDCourseCoreDataStorage {
     
     func deleteAllCourses(_ completionHandler: @escaping (MDOperationResultWithCompletion<Void>)) {
         
-        let operation: MDDeleteAllCoursesCoreDataStorageOperation = .init(managedObjectContext: self.managedObjectContext,
-                                                                          coreDataStack: self.coreDataStack,
-                                                                          coreDataStorage: self) { result in
-            completionHandler(result)
+        let operation: BlockOperation = .init {
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: CoreDataEntityName.CDCourseResponseEntity)
+            
+            let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+            
+            do {
+                
+                try self.managedObjectContext.execute(batchDeleteRequest)
+                
+                self.coreDataStack.save(managedObjectContext: self.managedObjectContext) { result in
+                    
+                    switch result {
+                        
+                    case .success:
+                        
+                        //
+                        completionHandler(.success(()))
+                        //
+                        
+                        //
+                        break
+                        //
+                        
+                    case .failure(let error):
+                        
+                        //
+                        completionHandler(.failure(error))
+                        //
+                        
+                        //
+                        break
+                        //
+                        
+                    }
+                    
+                }
+                
+                
+            } catch {
+                
+                //
+                completionHandler(.failure(error))
+                //
+                
+                //
+                return
+                //
+                
+            }
+            
         }
         
         //
