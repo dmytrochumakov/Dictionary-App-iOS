18b80909d8592b815149c385780122f810da80ea
Dima <dima.chumakovwork@gmail.com>
2021-11-10 12:01:48 +0200

    added empty MDCourseManager, MDWordManager

---
+++ MyDictionary_App_Swift/Core/Managers/MDCourseManager.swift
@@ -0,0 +1,24 @@
+//
+//  MDCourseManager.swift
+//  MyDictionary_App_Swift
+//
+//  Created by Dmytro Chumakov on 10.11.2021.
+//
+
+import Foundation
+
+protocol MDCourseManagerProtocol {
+    
+}
+
+final class MDCourseManager: MDCourseManagerProtocol {
+    
+    init() {
+        
+    }
+    
+    deinit {
+        debugPrint(#function, Self.self)
+    }
+    
+}
---
+++ MyDictionary_App_Swift/Core/Managers/MDWordManager.swift
@@ -0,0 +1,24 @@
+//
+//  MDWordManager.swift
+//  MyDictionary_App_Swift
+//
+//  Created by Dmytro Chumakov on 10.11.2021.
+//
+
+import Foundation
+
+protocol MDWordManagerProtocol {
+    
+}
+
+final class MDWordManager: MDWordManagerProtocol {
+    
+    init() {
+        
+    }
+    
+    deinit {
+        debugPrint(#function, Self.self)
+    }
+    
+}
