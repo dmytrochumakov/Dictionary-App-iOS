c66492c4f56867a6e84358a4b222d847f631eff2
Dima <dima.chumakovwork@gmail.com>
2021-11-10 12:48:05 +0200

    implemented create course method with check if course exists

--- MyDictionary_App_Swift/Core/Managers/MDCourseManager.swift
+++ MyDictionary_App_Swift/Core/Managers/MDCourseManager.swift
@@ -7,18 +7,86 @@
 
 import Foundation
 
-protocol MDCourseManagerProtocol {
+protocol MDCourseManagerProtocol: MDCreateCourseProtocol {
     
 }
 
 final class MDCourseManager: MDCourseManagerProtocol {
     
-    init() {
-        
+    fileprivate let courseCoreDataStorage: MDCourseCoreDataStorageProtocol
+    
+    init(courseCoreDataStorage: MDCourseCoreDataStorageProtocol) {
+        self.courseCoreDataStorage = courseCoreDataStorage
     }
     
     deinit {
         debugPrint(#function, Self.self)
+    }
+    
+}
+
+// MARK: - Create
+extension MDCourseManager {
+    
+    func createCourse(uuid: UUID,
+                      languageId: Int16,
+                      createdAt: Date,
+                      _ completionHandler: @escaping(MDOperationResultWithCompletion<CDCourseEntity>)) {
+        
+        courseCoreDataStorage.exists(byLanguageId: languageId) { [unowned self] existsResult in
+            
+            switch existsResult {
+                
+            case .success(let exists):
+                
+                if (exists) {
+                    
+                    //
+                    completionHandler(.failure(MDCourseOperationError.courseExists))
+                    //
+                    
+                    //
+                    return
+                    //
+                    
+                } else {
+                    
+                    //
+                    courseCoreDataStorage.createCourse(uuid: uuid,
+                                                       languageId: languageId,
+                                                       createdAt: createdAt) { createResult in
+                        
+                        //
+                        completionHandler(createResult)
+                        //
+                        
+                        //
+                        return
+                        //
+                        
+                    }
+                    //
+                    
+                }
+                
+                //
+                break
+                //
+                
+            case .failure(let error):
+                
+                //
+                completionHandler(.failure(error))
+                //
+                
+                //
+                break
+                //
+                
+            }
+            
+        }
+        
     }
     
 }
---
+++ MyDictionary_App_Swift/Core/Operations/Error/MDCourseOperationError.swift
@@ -0,0 +1,12 @@
+//
+//  MDCourseOperationError.swift
+//  MyDictionary_App_Swift
+//
+//  Created by Dmytro Chumakov on 10.11.2021.
+//
+
+import Foundation
+
+enum MDCourseOperationError: Error {
+    case courseExists
+}
