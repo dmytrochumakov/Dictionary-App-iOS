ce2b6910d58c615c0117651094cf7e7bc0cbdc9b
Dima <dima.chumakovwork@gmail.com>
2021-11-27 12:24:24 +0200

    separate MDReadWordProtocol interface also added MDReadAllWordsProtocol

--- MyDictionary_App_Swift/Core/Protocols/CRUD/Word/MDReadWordProtocol.swift
+++ MyDictionary_App_Swift/Core/Protocols/CRUD/Word/MDReadWordProtocol.swift
@@ -7,14 +7,22 @@
 
 import Foundation
 
-protocol MDReadWordProtocol {
+protocol MDReadWordByWordUUIDProtocol {
     
     func readWord(byWordUUID uuid: UUID,
                   _ completionHandler: @escaping(MDOperationResultWithCompletion<CDWordEntity>))
     
+}
+
+protocol MDReadWordByCourseUUIDAndWordTextProtocol {
+    
     func readWord(byCourseUUID courseUUID: UUID,
                   andWordText wordText: String,
                   _ completionHandler: @escaping(MDOperationResultWithCompletion<CDWordEntity>))
+    
+}
+
+protocol MDReadWordsByCourseUUIDAndFetchLimitAndFetchOffsetProtocol {
     
     func readWords(byCourseUUID uuid: UUID,
                    fetchLimit: Int,
@@ -22,13 +30,37 @@ protocol MDReadWordProtocol {
                    ascending: Bool,
                    _ completionHandler: @escaping(MDOperationsResultWithCompletion<CDWordEntity>))
     
+}
+
+protocol MDReadWordsWithFetchLimitAndFetchOffsetProtocol {
+    
     func readWords(fetchLimit: Int,
                    fetchOffset: Int,
                    ascending: Bool,
                    _ completionHandler: @escaping(MDOperationsResultWithCompletion<CDWordEntity>))
     
+}
+
+protocol MDReadAllWordsByCourseUUIDProtocol {
+    
     func readAllWords(byCourseUUID uuid: UUID,
                       ascending: Bool,
                       _ completionHandler: @escaping(MDOperationsResultWithCompletion<CDWordEntity>))
+    
+}
+
+protocol MDReadAllWordsProtocol {
+    
+    func readAllWords(ascending: Bool,
+                      _ completionHandler: @escaping(MDOperationsResultWithCompletion<CDWordEntity>))
+    
+}
+
+protocol MDReadWordProtocol: MDReadWordByWordUUIDProtocol,
+                             MDReadWordByCourseUUIDAndWordTextProtocol,
+                             MDReadWordsByCourseUUIDAndFetchLimitAndFetchOffsetProtocol,
+                             MDReadWordsWithFetchLimitAndFetchOffsetProtocol,
+                             MDReadAllWordsByCourseUUIDProtocol,
+                             MDReadAllWordsProtocol {
     
 }
